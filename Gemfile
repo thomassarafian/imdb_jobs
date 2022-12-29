23edc5c72bea0f748c34d5f459c309e1dedfa09d
Thomas Sarafian <thomassarafian@gmail.com>
2022-12-29 16:05:44 +0100

    add Devise

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :test do
 end
 
 gem "sidekiq", "~> 7.0"
+
+gem "devise", "~> 4.8"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  devise_for :users
   root "pages#home"
+  
   mount Sidekiq::Web => '/sidekiq'
 end
