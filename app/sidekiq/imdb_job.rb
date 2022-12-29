50347b64e6378dcc9bfaf83d879f15c6edcf9f97
Thomas Sarafian <thomassarafian@gmail.com>
2022-12-29 17:14:31 +0100

    get imdb api with faraday

--- app/sidekiq/imdb_job.rb
+++ app/sidekiq/imdb_job.rb
@@ -1,12 +1,14 @@
-require 'imdb'
-
 class ImdbJob
   include Sidekiq::Job
 
   def perform(*args)
-    p 'BEFORE IMDB'
-    search = Imdb::Search.new("The Godfather")
-    p search.movies.first.title
-    p 'AFTER IMDB'
+    conn = Faraday.new(url: "https://api.themoviedb.org/3/")
+    api_key = ENV["IMDB_KEY"]
+    response = conn.get do |req|
+      req.url "movie/76341"
+      req.params["api_key"] = api_key
+    end
+    response_body = JSON.parse(response.body)
+    p response_body['original_title']
   end
 end
