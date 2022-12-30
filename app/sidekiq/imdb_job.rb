65c61d658ff12982c4d4f64e4db42911981cd75a
Thomas Sarafian <thomassarafian@gmail.com>
2022-12-30 12:47:03 +0100

    clean jobs

--- app/sidekiq/imdb_job.rb
+++ app/sidekiq/imdb_job.rb
@@ -2,13 +2,6 @@ class ImdbJob
   include Sidekiq::Job
 
   def perform(*args)
-    conn = Faraday.new(url: "https://api.themoviedb.org/3/")
-    api_key = ENV["IMDB_KEY"]
-    response = conn.get do |req|
-      req.url "movie/76341"
-      req.params["api_key"] = api_key
-    end
-    response_body = JSON.parse(response.body)
-    Movie.create! title: response_body['original_title'], description: response_body['overview']
+
   end
 end
